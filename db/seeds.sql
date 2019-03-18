d1f41ee704a451798f289f9140372f165a6fb5b4
Jacqueline Gwynn <jacquelinegwynn@Jacquelines-MacBook-Air.local>
2019-03-18 18:21:48 -0400

    add db folder

---
+++ db/seeds.sql
@@ -0,0 +1,25 @@
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Cheese Burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Hamburger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Causan Chicken Burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Vega Burger');
+
+INSERT INTO burgers
+    (burger_name)
+VALUES
+    ('Turkey Burger'); 
+
---
+++ server.js
@@ -0,0 +1,3 @@
+const express = require("express"); 
+const hdbs = require("express-handlebars"); 
+const sql = require("mysql"); 
