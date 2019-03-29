1aa02bac6ca693011df89edd6894d476ca9cff7b
Jacqueline Gwynn <jacquelinegwynn@Jacquelines-MacBook-Air.local>
2019-03-28 21:21:32 -0400

    update orm to help query

--- config/orm.js
+++ config/orm.js
@@ -1,26 +1,40 @@
 const connection = require("./connection.js");
 
 const orm = {
-    selectAll: function (cb) {
-        const queryString = "SELECT * FROM burgers;";
+    selectAll: function (tableInput, cb) {
+        const queryString = 'SELECT * FROM ' + tableInput + ' ;';
         connection.query(queryString, function (err, result) {
             if (err) throw err;
 
             cb(result)
         });
     },
-    insertOne: function (burger) {
-        const queryString = "INSERT ?? INTO burgers";
-        connection.query(queryString, function (err, result) {
+    insertOne: function (table,cols, val, cb) {
+        const queryString = "INSERT INTO " + table ;
+        queryString += ' ('; 
+        queryString += cols.toString(); 
+        queryString += ' )'; 
+        queryString += 'VALUES ('; 
+        queryString += printQuestionMarks(vals.length); 
+        queryString += ') '; 
+        
+        console.log(queryString); 
+
+        connection.query(queryString,vals, function (err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     },
-    updateOne: function (name, id) {
-        const queryString = "UPDATE burger_name SET =??, WHERE id = ?";
+    updateOne: function (table,objColVals,condition, cb) {
+        const queryString = 'UPDATE ' + table;
+        queryString += ' SET '; 
+        queryString += objToSql(objColVals); 
+        queryString += ' WHERE '; 
+        queryString += condition; 
+        
         connection.query(queryString, function (err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         });
     }
 
